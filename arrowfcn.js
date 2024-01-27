79ce00547f878e044372f0db233d78e02cd19bc3
Kathy Quach <kathyquach@MacBook-Air.local>
2024-01-27 01:16:21 -0800

    arrow functions

---
+++ arrowfcn.js
@@ -0,0 +1,25 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//       return val * 2;
+//     });
+//   }
+
+
+arr = [1,2,3,4];
+numbers = [1,2,3,4];
+
+const double = arr => arr.map(val => val*2);
+
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+const squareAndFindEvens = numbers => numbers.map(val => val ** 2).filter(square => square % 2 === 0)
+// console.log(squareAndFindEvens(numbers));
